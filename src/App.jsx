04a527b723990df215c75e2ee7bcd645641b32da
O_had <ohadkachlon@gmail.com>
2023-03-04 12:41:53 +0200

    starter React Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css"
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <div>
+       <h1 className='bg-red-600'>This</h1>
+       <p>starter for Vite React and Tailwind by Ohad 2023</p>
+    </div>
+    </div>
+  )
+}
+
+export default App
